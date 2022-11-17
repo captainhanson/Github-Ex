4acc300d0753116e78a86f482b0b559b52c72524
Hanson Siverts <hansoncaptain@gmail.com>
2022-11-16 20:45:40 -0800

    Initial Commit

---
+++ Github Ex/Github_ExApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExApp.swift
+//  Github Ex
+//
+//  Created by Hanson Siverts on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
